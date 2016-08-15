0263b14835b7c5fe50665fcac119648b60254bc9
HubQC <staranderson1900@gmail.com>
2016-08-15 16:12:39 -0400

    Add footer tag to footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,10 @@
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
+        <footer class="footer">
+            <div class="container">
+                <p class="text-muted text-center">TODO footer</p>
+            </div>
+        </footer>
     </body>
 </html>
