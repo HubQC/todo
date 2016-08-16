c89a1e2c284ee63c0df13d1c34e8e5dcdc165fd0
HubQC <staranderson1900@gmail.com>
2016-08-16 13:29:36 -0400

    add  to footer view

--- views/footer.php
+++ views/footer.php
@@ -1,10 +1,13 @@
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
-        <footer class="footer">
+        <footer class="footer" style="margin-top: 20%;">
             <div class="container">
                 <p class="text-muted text-center">TODO footer</p>
             </div>
+            <?php
+                var_dump($_SESSION);
+            ?>
         </footer>
     </body>
 </html>
